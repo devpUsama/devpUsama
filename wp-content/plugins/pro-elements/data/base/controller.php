c88b96ef04ab8db58ec34fa066cb08abc47e73fb
Muhammad Usama <113655524+devpUsama@users.noreply.github.com>
2023-02-09 07:17:04 +0500

    V2

---
+++ wp-content/plugins/pro-elements/data/base/controller.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Class &quot;Elementor\Data\Base\Controller&quot; not found in C:\xampp\htdocs\devpusama\wp-content\plugins\pro-elements\data\base\controller.php:6
+Stack trace:
+#0 {main}
+  thrown in <b>C:\xampp\htdocs\devpusama\wp-content\plugins\pro-elements\data\base\controller.php</b> on line <b>6</b><br>
